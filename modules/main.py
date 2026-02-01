1b2015c694166e83466a65ec18528e5584718ca8
Andrei Hryshechkin <andrei.hryshechkin@gmail.com>
2026-02-01 17:06:35 +0100

    feat: add Component class

--- modules/main.py
+++ modules/main.py
@@ -6,10 +6,10 @@ from modules.sample import Samples
 from modules.view import View
 
 
-engine = Engine()
 for sample in Samples.get_pairs():
     src = sample["src"].strip().splitlines()
     trg = sample["trg"].strip().splitlines()
+    engine = Engine()
     engine.run(src, trg)
     view = View(sample)
     view.show_report()
