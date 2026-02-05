34b2d1804d1ef1a4577afc25481f89800a1d313c
Andrei Hryshechkin <andrei.hryshechkin@gmail.com>
2026-02-05 20:08:00 +0100

    feat: move estimation logic from decision to component

--- modules/component.py
+++ modules/component.py
@@ -8,3 +8,14 @@ class Component:
         self.links.add(link)
         self.bricks.add(link.left)
         self.bricks.add(link.right)
+
+
+    def evaluate(self, heuristics):
+        scores = dict()
+        for link in self.links:
+            score = 0
+            for heuristic in heuristics:
+                score += heuristic.score(link)
+            scores[link] = score
+
+        return scores
