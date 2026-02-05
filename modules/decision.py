11266192f21aa7f67e894665c72bd839bf0aa1f4
Andrei Hryshechkin <andrei.hryshechkin@gmail.com>
2026-02-05 20:08:00 +0100

    feat: move estimation logic from decision to component

--- modules/decision.py
+++ modules/decision.py
@@ -5,16 +5,11 @@ class Decision:
 
 
     def make(self):
-        scores = dict()
-        for link in self.component.links:
-            score = 0
-            for heuristic in self.heuristics:
-                score += heuristic.score(link)
-            scores[link] = score
-
         result = dict()
-        sorted_links = sorted(scores, key=scores.get, reverse=True)
         used_bricks = set()
+
+        scores = self.component.evaluate(self.heuristics)
+        sorted_links = sorted(scores, key=scores.get, reverse=True)
         for link in sorted_links:
             if link.left not in used_bricks and link.right not in used_bricks:
                 used_bricks.add(link.left)
