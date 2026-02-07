524de746e755db9caf060dd36d710963a225b86d
Andrei Hryshechkin <andrei.hryshechkin@gmail.com>
2026-02-07 09:41:33 +0100

    refactor: split Link into separate files and subclasses

--- modules/model/link/scored_link.py
+++ modules/model/link/scored_link.py
@@ -1,18 +1,4 @@
-class Link:
-    def __init__(self, rule_id, left, right):
-        self.rule_id = rule_id
-        self.left = left
-        self.right = right
-
-
-    def __str__(self):
-        return (
-            f"{self.rule_id.value:>9} | #### | {self.left} #### | {self.right}"
-        )
-
-
-    def __repr__(self):
-        return f"Link(rule_id={self.rule_id.value})"
+from .base import Link
 
 
 class ScoredLink(Link):
