c9b73d0eccaccaed830912d59aa140c11a16628a
Andrei Hryshechkin <andrei.hryshechkin@gmail.com>
2026-02-22 21:52:37 +0100

    test: add setUp method

---
+++ tests/component/test_basic_scenarios.py
@@ -0,0 +1,16 @@
+import unittest
+
+from application import Pipeline, ScenarioCatalog
+
+
+class TestSimpleScenarios(unittest.TestCase):
+    def setUp(self):
+        self.catalog = ScenarioCatalog()
+
+
+    def test_sce_014_alter_constraint_null(self):
+        scenario = self.catalog.get(["014"])[0]
+        pipeline = Pipeline(scenario.before, scenario.after)
+        result = pipeline.run()
+
+        self.assertTrue(True)
