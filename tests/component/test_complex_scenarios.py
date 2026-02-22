4f034865138767c1729d5b10a7bc9dd3fb3a81e4
Andrei Hryshechkin <andrei.hryshechkin@gmail.com>
2026-02-22 21:52:37 +0100

    test: add setUp method

--- tests/component/test_complex_scenarios.py
+++ tests/component/test_complex_scenarios.py
@@ -4,18 +4,20 @@ from application import Pipeline, ScenarioCatalog
 
 
 class TestComplexScenarios(unittest.TestCase):
-    def test_sce_014_alter_constraint_null(self):
-        scenario_catalog = ScenarioCatalog()
-        scenario = scenario_catalog.get(["014"])[0]
+    def setUp(self):
+        self.catalog = ScenarioCatalog()
+
+
+    def test_sce_051_alter_column_then_add_another_after(self):
+        scenario = self.catalog.get(["051"])[0]
         pipeline = Pipeline(scenario.before, scenario.after)
         result = pipeline.run()
 
         self.assertTrue(True)
 
 
-    def test_sce_051_alter_column_then_add_another_after(self):
-        scenario_catalog = ScenarioCatalog()
-        scenario = scenario_catalog.get(["051"])[0]
+    def test_sce_151_alter_column_then_add_another_after(self):
+        scenario = self.catalog.get(["151"])[0]
         pipeline = Pipeline(scenario.before, scenario.after)
         result = pipeline.run()
 
