98024f33697a128f5704f90c88e760a19402a859
Andrei Hryshechkin <andrei.hryshechkin@gmail.com>
2026-02-22 10:58:41 +0100

    feat: extract orchestration into a separate class

--- infrastructure/cli/main.py
+++ infrastructure/cli/main.py
@@ -1,5 +1,5 @@
-from application import Engine
-from domain import *
+from application import Pipeline
+from domain import Samples
 from infrastructure import View
 
 
@@ -7,33 +7,16 @@ for sample in Samples.get_pairs():
     src = sample["src"].strip().splitlines()
     trg = sample["trg"].strip().splitlines()
 
-    engine = Engine()
-    engine.build_bricks(src, trg)
-
-    filtered_bricks = Filter(engine.bricks)
-    bricks_left = filtered_bricks.by_side(Side.LEFT)
-    bricks_right = filtered_bricks.by_side(Side.RIGHT)
-    matcher = Matcher(bricks_left.bricks, bricks_right.bricks)
-    links = list()
-    for rule in [RuleName(), RulePosition(), RuleSignature(), RuleDelete(), RuleInsert(), RuleOverflow()]:
-        links.extend(matcher.match(rule))
-
-    components = engine.build_components(links)
-
-    selected_links, all_links = list(), list()
-    heuristics = [HeuristicCompatibility(), HeuristicName(), HeuristicPosition(), HeuristicSignature()]
-    for component in components:
-        all_links.append(component.evaluate(heuristics))
-        decision = Decision(component, heuristics)
-        selected_links.append(decision.make())
+    pipeline = Pipeline(src, trg)
+    result = pipeline.run()
 
     view = View(sample)
     view.show_report()
-    view.show_bricks(engine.bricks)
-    view.show_links(links)
-    view.show_components(components)
-    view.show_decisions(all_links)
-    view.show_decisions(selected_links)
+    view.show_bricks(result[0])
+    view.show_links(result[1])
+    view.show_components(result[2])
+    view.show_decisions(result[3])
+    view.show_decisions(result[4])
 
     # print("")
     # for case in Samples.list_cases():
