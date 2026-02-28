40ffd5bd02388ecd8d146a66be5f12bbc90cda22
Andrei Hryshechkin <andrei.hryshechkin@gmail.com>
2026-02-28 23:06:01 +0100

    test: finalize scenario migration

--- scenarios/sce_703_swap_columns_with_overlap/after.sql
+++ scenarios/sce_703_swap_columns_with_overlap/after.sql
@@ -1,10 +1,20 @@
 col_01 int not null
-col_06 int
-col_03 varchar(200)
-col_09 datetime2 not null
-col_05 varchar(50)
 col_02 varchar(50) not null
-col_07 varchar(1) not null
-col_08 date not null
+col_12 varchar(50) not null
 col_04 varchar(50) not null
+col_05 varchar(50)
+col_06 int
+col_16 int
+col_08 date not null
+col_09 datetime2 not null
 col_10 datetime2 not null
+col_11 int not null
+col_03 varchar(200)
+col_13 varchar(200)
+col_14 varchar(50) not null
+col_15 varchar(50)
+col_07 varchar(1) not null
+col_17 varchar(1) not null
+col_18 date not null
+col_19 datetime2 not null
+col_20 datetime2 not null
