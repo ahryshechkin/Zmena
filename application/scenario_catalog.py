c6ecf51f6fa7332e093ab01864e531e2df2557f4
Andrei Hryshechkin <andrei.hryshechkin@gmail.com>
2026-03-01 15:12:02 +0100

    feat: extend Scenario class with two attributes

--- application/scenario_catalog.py
+++ application/scenario_catalog.py
@@ -11,11 +11,14 @@ class ScenarioCatalog:
     def scenario(self, sce_id):
         for path in self.root_dir.iterdir():
             if sce_id in path.name:
+                sce_id, name = path.name.split("_", maxsplit=2)[1:]
                 before = (path / "before.sql").read_text(encoding="utf-8")
                 after = (path / "after.sql").read_text(encoding="utf-8")
                 expected = (path / "expected.txt").read_text(encoding="utf-8")
 
                 return Scenario(
+                    sce_id=sce_id,
+                    name=name,
                     before=before.splitlines(),
                     after=after.splitlines(),
                     expected=expected.splitlines(),
@@ -29,11 +32,14 @@ class ScenarioCatalog:
 
         for path in self.root_dir.iterdir():
             if any(sce_id in path.name for sce_id in sce_ids):
+                sce_id, name = path.name.split("_", maxsplit=2)[1:]
                 before = (path / "before.sql").read_text(encoding="utf-8")
                 after = (path / "after.sql").read_text(encoding="utf-8")
                 expected = (path / "expected.txt").read_text(encoding="utf-8")
 
                 scenarios.append(Scenario(
+                    sce_id=sce_id,
+                    name=name,
                     before=before.splitlines(),
                     after=after.splitlines(),
                     expected=expected.splitlines(),
