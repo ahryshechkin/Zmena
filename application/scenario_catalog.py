8f17b4b14696664fbf45e61399ad5fbd7de1e044
Andrei Hryshechkin <andrei.hryshechkin@gmail.com>
2026-03-01 19:19:58 +0100

    feat: implement method to return all scenarios

--- application/scenario_catalog.py
+++ application/scenario_catalog.py
@@ -8,45 +8,45 @@ class ScenarioCatalog:
         self.root_dir = Path(__file__).resolve().parents[1] / "scenarios"
 
 
-    def scenario(self, sce_id):
+    def build_scenario_from(self, path):
+        sce_id, name = path.name.split("_", maxsplit=2)[1:]
+        before = (path / "before.sql").read_text(encoding="utf-8")
+        after = (path / "after.sql").read_text(encoding="utf-8")
+        expected = (path / "expected.txt").read_text(encoding="utf-8")
+
+        return Scenario(
+            sce_id=sce_id,
+            name=name,
+            before=before.splitlines(),
+            after=after.splitlines(),
+            expected=expected.splitlines(),
+        )
+
+
+    def get(self, sce_id):
         for path in self.root_dir.iterdir():
             if sce_id in path.name:
-                sce_id, name = path.name.split("_", maxsplit=2)[1:]
-                before = (path / "before.sql").read_text(encoding="utf-8")
-                after = (path / "after.sql").read_text(encoding="utf-8")
-                expected = (path / "expected.txt").read_text(encoding="utf-8")
-
-                return Scenario(
-                    sce_id=sce_id,
-                    name=name,
-                    before=before.splitlines(),
-                    after=after.splitlines(),
-                    expected=expected.splitlines(),
-                )
+                return self.build_scenario_from(path)
 
         return None
 
 
-    def scenarios(self, sce_ids):
+    def get_many(self, sce_ids):
         scenarios = list()
 
         for path in self.root_dir.iterdir():
             if any(sce_id in path.name for sce_id in sce_ids):
-                sce_id, name = path.name.split("_", maxsplit=2)[1:]
-                before = (path / "before.sql").read_text(encoding="utf-8")
-                after = (path / "after.sql").read_text(encoding="utf-8")
-                expected = (path / "expected.txt").read_text(encoding="utf-8")
-
-                scenarios.append(Scenario(
-                    sce_id=sce_id,
-                    name=name,
-                    before=before.splitlines(),
-                    after=after.splitlines(),
-                    expected=expected.splitlines(),
-                ))
+                scenario = self.build_scenario_from(path)
+                scenarios.append(scenario)
 
         return scenarios
 
 
-    def all(self):
-        pass
+    def get_all(self):
+        scenarios = list()
+
+        for path in self.root_dir.iterdir():
+            scenario = self.build_scenario_from(path)
+            scenarios.append(scenario)
+
+        return scenarios
