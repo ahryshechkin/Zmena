e66403a756a59b450d5fb1c4b3c2c6c5302de2f2
Andrei Hryshechkin <andrei.hryshechkin@gmail.com>
2026-03-01 19:19:58 +0100

    feat: implement method to return all scenarios

--- infrastructure/cli/main.py
+++ infrastructure/cli/main.py
@@ -2,19 +2,20 @@ from application import Pipeline, ScenarioCatalog
 from infrastructure import View
 
 
-sce_ids = ["501"]
+sce_ids = ["301", "304"]
 catalog = ScenarioCatalog()
-for scenario in catalog.scenarios(sce_ids):
+# for scenario in catalog.get_many(sce_ids):
+for scenario in catalog.get_all():
     pipeline = Pipeline(scenario.before, scenario.after)
     result = pipeline.run()
 
     view = View(scenario)
     view.show_report()
-    view.show_bricks(result["bricks"])
-    view.show_links(result["links"])
-    view.show_components(result["components"])
+    # view.show_bricks(result["bricks"])
+    # view.show_links(result["links"])
+    # view.show_components(result["components"])
     # view.show_decisions(result["all_links"])
-    view.show_decisions(result["selected_links"])
+    # view.show_decisions(result["selected_links"])
 
 
     # print("")
