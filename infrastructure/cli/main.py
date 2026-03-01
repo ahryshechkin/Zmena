cf2ee6acb8543601a44f5f30a1acdfc0a0a02a74
Andrei Hryshechkin <andrei.hryshechkin@gmail.com>
2026-03-01 15:41:14 +0100

    refactor: clean up main workflow

--- infrastructure/cli/main.py
+++ infrastructure/cli/main.py
@@ -1,5 +1,4 @@
 from application import Pipeline, ScenarioCatalog
-from domain import Samples
 from infrastructure import View
 
 
@@ -12,22 +11,11 @@ for scenario in catalog.scenarios(sce_ids):
     view = View(scenario)
     view.show_report()
     view.show_bricks(result["bricks"])
-
-
-# for sample in Samples.get_pairs():
-#     src = sample["src"].strip().splitlines()
-#     trg = sample["trg"].strip().splitlines()
-#
-#     pipeline = Pipeline(src, trg)
-#     result = pipeline.run()
-#
-#     view = View(sample)
-#     view.show_report()
-    # view.show_bricks(result["bricks"])
-    # view.show_links(result["links"])
-    # view.show_components(result["components"])
+    view.show_links(result["links"])
+    view.show_components(result["components"])
     # view.show_decisions(result["all_links"])
-    # view.show_decisions(result["selected_links"])
+    view.show_decisions(result["selected_links"])
+
 
     # print("")
     # for case in Samples.list_cases():
