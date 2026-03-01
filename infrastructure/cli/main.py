ca06f8e4113c4233a734523a36f22f65708f5850
Andrei Hryshechkin <andrei.hryshechkin@gmail.com>
2026-03-01 12:41:35 +0100

    feat: bring scenario catalog into main workflow

--- infrastructure/cli/main.py
+++ infrastructure/cli/main.py
@@ -1,22 +1,32 @@
-from application import Pipeline
+from application import Pipeline, ScenarioCatalog
 from domain import Samples
 from infrastructure import View
 
 
-for sample in Samples.get_pairs():
-    src = sample["src"].strip().splitlines()
-    trg = sample["trg"].strip().splitlines()
+sce_ids = ["", ""]
+catalog = ScenarioCatalog()
+for scenario in catalog.scenarios(sce_ids):
+    pipeline = Pipeline(scenario.before, scenario.after)
+    result = pipeline.run()git
 
-    pipeline = Pipeline(src, trg)
-    result = pipeline.run()
+    # view = View(sample)
+    # view.show_report()
 
-    view = View(sample)
-    view.show_report()
+
+# for sample in Samples.get_pairs():
+#     src = sample["src"].strip().splitlines()
+#     trg = sample["trg"].strip().splitlines()
+#
+#     pipeline = Pipeline(src, trg)
+#     result = pipeline.run()
+#
+#     view = View(sample)
+#     view.show_report()
     # view.show_bricks(result["bricks"])
     # view.show_links(result["links"])
     # view.show_components(result["components"])
     # view.show_decisions(result["all_links"])
-    view.show_decisions(result["selected_links"])
+    # view.show_decisions(result["selected_links"])
 
     # print("")
     # for case in Samples.list_cases():
