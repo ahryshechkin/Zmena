cb54d2a100a87fc16e030992addb7ee75c4dcea0
Andrei Hryshechkin <andrei.hryshechkin@gmail.com>
2026-03-02 20:37:45 +0100

    test: update method name to line up with new class interface

--- tests/component/test_basic_scenarios.py
+++ tests/component/test_basic_scenarios.py
@@ -9,7 +9,7 @@ class TestBasicScenarios(unittest.TestCase):
 
 
     def test_sce_011_add_column_not_null(self):
-        scenario = self.catalog.scenario("011")
+        scenario = self.catalog.get("011")
         pipeline = Pipeline(scenario.before, scenario.after)
         result = pipeline.run()
 
@@ -21,7 +21,7 @@ class TestBasicScenarios(unittest.TestCase):
 
 
     def test_sce_012_add_column_null(self):
-        scenario = self.catalog.scenario("012")
+        scenario = self.catalog.get("012")
         pipeline = Pipeline(scenario.before, scenario.after)
         result = pipeline.run()
 
@@ -33,7 +33,7 @@ class TestBasicScenarios(unittest.TestCase):
 
 
     def test_sce_013_alter_constraint_not_null(self):
-        scenario = self.catalog.scenario("013")
+        scenario = self.catalog.get("013")
         pipeline = Pipeline(scenario.before, scenario.after)
         result = pipeline.run()
 
@@ -45,7 +45,7 @@ class TestBasicScenarios(unittest.TestCase):
 
 
     def test_sce_014_alter_constraint_null(self):
-        scenario = self.catalog.scenario("014")
+        scenario = self.catalog.get("014")
         pipeline = Pipeline(scenario.before, scenario.after)
         result = pipeline.run()
 
@@ -57,7 +57,7 @@ class TestBasicScenarios(unittest.TestCase):
 
 
     def test_sce_015_change_data_type(self):
-        scenario = self.catalog.scenario("015")
+        scenario = self.catalog.get("015")
         pipeline = Pipeline(scenario.before, scenario.after)
         result = pipeline.run()
 
@@ -69,7 +69,7 @@ class TestBasicScenarios(unittest.TestCase):
 
 
     def test_sce_016_drop_column(self):
-        scenario = self.catalog.scenario("016")
+        scenario = self.catalog.get("016")
         pipeline = Pipeline(scenario.before, scenario.after)
         result = pipeline.run()
 
@@ -81,7 +81,7 @@ class TestBasicScenarios(unittest.TestCase):
 
 
     def test_sce_017_move_column(self):
-        scenario = self.catalog.scenario("017")
+        scenario = self.catalog.get("017")
         pipeline = Pipeline(scenario.before, scenario.after)
         result = pipeline.run()
 
