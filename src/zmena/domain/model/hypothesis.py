b84acc947b8f66b331507e0f2e1ffea994cddff7
Andrei Hryshechkin <andrei.hryshechkin@gmail.com>
2026-03-19 21:34:07 +0100

    feat: implement Hypothesis class

--- src/zmena/domain/model/hypothesis.py
+++ src/zmena/domain/model/hypothesis.py
@@ -1,3 +1,14 @@
 class Hypothesis:
-    def __init__(self):
-        pass
+    def __init__(self, rule_id, left, right):
+        self.rule_id = rule_id
+        self.left = left
+        self.right = right
+
+    def __str__(self):
+        return f"{self.rule_id.value:>9} | #### | {self.left} #### | {self.right}"
+
+    def __repr__(self):
+        return f"Hypothesis(rule_id={self.rule_id.value})"
+
+    def key(self):
+        return self.left, self.right
