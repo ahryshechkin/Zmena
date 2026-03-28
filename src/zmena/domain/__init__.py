58657bcf697ae84ff94b6b572bfece9809dcd39a
Andrei Hryshechkin <andrei.hryshechkin@gmail.com>
2026-03-28 22:10:22 +0100

    refactor: clean up imports

--- src/zmena/domain/__init__.py
+++ src/zmena/domain/__init__.py
@@ -1,16 +1,7 @@
 from zmena.domain.model.hunk import Hunk
 from zmena.domain.model.span import Span
 
-from .model import (
-    BrickLeft,
-    BrickRight,
-    BrickStub,
-)
-
 __all__ = [
-    "BrickLeft",
-    "BrickRight",
-    "BrickStub",
     "Hunk",
     "Span",
 ]
--- src/zmena/domain/model/brick/__init__.py
+++
@@ -1,9 +0,0 @@
-from .left import BrickLeft
-from .right import BrickRight
-from .stub import BrickStub
-
-__all__ = [
-    "BrickLeft",
-    "BrickRight",
-    "BrickStub",
-]
