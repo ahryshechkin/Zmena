ce5760debf3cf7e29317c2cb0eb27f2809418223
Andrei Hryshechkin <andrei.hryshechkin@gmail.com>
2026-03-29 11:40:18 +0200

    refactor: rename Evidence attribute

--- src/zmena/domain/model/evidence.py
+++ src/zmena/domain/model/evidence.py
@@ -1,8 +1,8 @@
 class Evidence:
-    def __init__(self, heuristic, hypothesis, score):
-        self.heuristic = heuristic
+    def __init__(self, heuristic_label, hypothesis, score):
+        self.heuristic_label = heuristic_label
         self.hypothesis = hypothesis
         self.score = score
 
     def __repr__(self):
-        return f"Evidence(heuristic={self.heuristic},score={self.score})"
+        return f"Evidence(heuristic={self.heuristic_label},score={self.score})"
