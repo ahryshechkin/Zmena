2431e7001e7f108b4015dab2fac73d69519d6365
Andrei Hryshechkin <andrei.hryshechkin@gmail.com>
2026-04-01 22:34:20 +0200

    feat: provide __repr__ for Decision

--- src/zmena/infrastructure/view/view.py
+++ src/zmena/infrastructure/view/view.py
@@ -117,7 +117,7 @@ class View:
             print(
                 f"\n#### Component {i}: "
                 f"hypotheses={len(component.hypotheses)}, bricks={len(component.bricks)} "
-                f"{'#' * 140} \n"
+                f"{'#' * 138} \n"
                 f"{'rule':>9} | "
                 f"{'####'} | "
                 f"{'tag':>7} | {'side':>4} | {'segment':<8} | {'position':>8} | "
@@ -142,7 +142,7 @@ class View:
             print(
                 f"\n#### Component {i}: "
                 f"hypotheses={len(component.hypotheses)}, bricks={len(component.bricks)} "
-                f"{'#' * 36} \n"
+                f"{'#' * 34} \n"
                 f"{'tag':>7} | {'side':>4} | {'segment':<8} | {'position':>8} | "
                 f"{'name':<7} | {'type':<13} | {'constraint':<10} | ",
             )
