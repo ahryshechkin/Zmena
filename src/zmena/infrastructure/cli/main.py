09792c0b87c006d9f1f300131fcdc50cfdbf0627
Andrei Hryshechkin <andrei.hryshechkin@gmail.com>
2026-04-04 14:17:34 +0200

    feat: compose decision report from links

--- src/zmena/infrastructure/cli/main.py
+++ src/zmena/infrastructure/cli/main.py
@@ -1,5 +1,8 @@
 from zmena.application import Pipeline, ScenarioCatalog
-from zmena.infrastructure.view.report_link import ReportLink
+from zmena.infrastructure.view.brick_report import BrickReport
+from zmena.infrastructure.view.component_report import ComponentReport
+from zmena.infrastructure.view.hypothesis_report import HypothesisReport
+from zmena.infrastructure.view.report_decision import ReportDecision
 from zmena.infrastructure.view.view import View
 
 sce_ids = ["313"]
@@ -8,17 +11,19 @@ for scenario in catalog.get_many(sce_ids):
     pipeline = Pipeline(scenario.before, scenario.after)
     result = pipeline.run()
 
-    # BrickReport(result["bricks"]).render()
-    # HypothesisReport(result["hypotheses"]).render()
-    # ComponentReport(result["components"]).render()
-    ReportLink(result["decisions"][0].chosen()).render()
-
     view = View(scenario)
-    # view.show_report()
+    view.show_report()
+
+    BrickReport(result["bricks"]).render()
+    HypothesisReport(result["hypotheses"]).render()
+    ComponentReport(result["components"]).render()
+    # ReportLink(result["decisions"][0].chosen()).render()
+    ReportDecision(result["decisions"]).render()
+
     # view.show_bricks(result["bricks"])
     # view.show_hypotheses(result["hypotheses"])
     # view.show_components(result["components"])
-    view.show_decisions(result["decisions"])
+    # view.show_decisions(result["decisions"])
 
 
 # print("")
