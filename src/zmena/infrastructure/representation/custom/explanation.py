0e9aea2c864eabbdb7fbbe11c89ebf6b701f8331
Andrei Hryshechkin <andrei.hryshechkin@gmail.com>
2026-04-11 15:24:07 +0200

    feat: format evidence

--- src/zmena/infrastructure/representation/custom/explanation.py
+++ src/zmena/infrastructure/representation/custom/explanation.py
@@ -7,23 +7,28 @@ class ReportExplanation:
         self.color = Color()
 
     def render(self):
+        print()
         for link in self.explanation.links:
             left, right = link.bricks
 
             evidences = []
-            for evi in link.evidences:
-                filler = " "
-                if evi.score >= 0:
-                    sign = "+"
-                    mark = self.color.colorize_sign(sign, "✔")
-                else:
-                    sign = "-"
-                    mark = self.color.colorize_sign(sign, "✖")
-                evidence = f"{filler:<3}{mark}{sign:>3}{abs(evi.score):<4}{evi.reason}\n"
-                evidences.append(evidence)
+            for evidence in link.evidences:
+                evi = self.format(evidence)
+                evidences.append(evi)
 
             print(
-                f"Link: {left.name} -> {right.name}\n"
+                f"Link: "
+                f"{left.name} ({left.side}:{left.position})"
+                f" -> "
+                f"{right.name} ({right.side}:{right.position})\n"
                 f"Score: {link.score}\n"
                 f"Evidences:\n{''.join(evidences)}"
             )
+
+    def format(self, evidence):
+        filler = " "
+
+        sign = "+" if evidence.score >= 0 else "-"
+        mark = self.color.style_sign(sign)
+
+        return f"{filler:<3}{mark}{sign:>3}{abs(evidence.score):<4}{evidence.reason}\n"
