fd1d030f6310c33aea142508bcd99dbb768e777c
Andrei Hryshechkin <andrei.hryshechkin@gmail.com>
2026-04-11 14:46:32 +0200

    feat: colorize sign indicator

--- src/zmena/infrastructure/representation/color.py
+++ src/zmena/infrastructure/representation/color.py
@@ -1,9 +1,27 @@
-from enum import StrEnum
+from zmena.domain.types.tag import Tag
 
 
-class Color(StrEnum):
+class Color:
     GRAY = "\033[38;5;250m"
     RED = "\033[91m"
     GREEN = "\033[92m"
     YELLOW = "\033[93m"
     RESET = "\033[0m"
+
+    def colorize_text(self, tag, text):
+        colors = {
+            Tag.DELETE: self.RED,
+            Tag.EQUAL: self.GRAY,
+            Tag.INSERT: self.GREEN,
+            Tag.REPLACE: self.YELLOW,
+        }
+
+        return f"{colors[tag].value}{text}{self.RESET.value}"
+
+    def colorize_sign(self, sign, text):
+        colors = {
+            "-": self.RED,
+            "+": self.GREEN,
+        }
+
+        return f"{colors[sign]}{text}{self.RESET}"
