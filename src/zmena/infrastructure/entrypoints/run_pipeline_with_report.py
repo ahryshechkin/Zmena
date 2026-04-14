0fe63f01bdc72c3dbe3acc4a236c886ba3c116f6
Andrei Hryshechkin <andrei.hryshechkin@gmail.com>
2026-04-14 21:11:50 +0200

    docs: update HoC badge

--- src/zmena/infrastructure/entrypoints/run_pipeline_with_report.py
+++ src/zmena/infrastructure/entrypoints/run_pipeline_with_report.py
@@ -1,7 +1,7 @@
 from zmena.application import Pipeline, ScenarioCatalog
 from zmena.infrastructure.representation.report import Report
 
-sce_ids = ["703"]
+sce_ids = ["313"]
 catalog = ScenarioCatalog()
 for scenario in catalog.get_many(sce_ids):
     pipeline = Pipeline(scenario.before, scenario.after)
@@ -10,6 +10,6 @@ for scenario in catalog.get_many(sce_ids):
     report = Report(scenario)
     report.show_scenario()
     # report.show_bricks(result["bricks"])
-    # report.show_hypotheses(result["hypotheses"])
+    report.show_hypotheses(result["hypotheses"])
     # report.show_components(result["components"])
     report.show_decisions(result["decisions"])
