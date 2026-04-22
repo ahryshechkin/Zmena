26187d6ef0c471779bb74e2ca8b065cc92ffe9c6
Andrei Hryshechkin <andrei.hryshechkin@gmail.com>
2026-04-22 21:26:18 +0200

    feat: add methods to FragmentBundle to hide fragment implementation from Imbalance rule

--- src/zmena/domain/model/fragment_bundle.py
+++ src/zmena/domain/model/fragment_bundle.py
@@ -13,3 +13,17 @@ class FragmentBundle:
 
     def right(self):
         return [f for f in self.fragments if f.side == Side.RIGHT]
+
+    def left_by_segment(self):
+        fragments = {}
+        for fragment in self.left():
+            fragments.setdefault(fragment.segment, []).append(fragment)
+
+        return fragments
+
+    def right_by_segment(self):
+        fragments = {}
+        for fragment in self.right():
+            fragments.setdefault(fragment.segment, []).append(fragment)
+
+        return fragments
