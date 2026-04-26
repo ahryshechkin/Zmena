535148645d2588accf2c0104b30d64ff09cb445a
Andrei Hryshechkin <andrei.hryshechkin@gmail.com>
2026-04-26 19:24:53 +0200

    refactor: remove print method from ScenarioCatalog

--- src/zmena/infrastructure/adapters/scenario_catalog.py
+++ src/zmena/infrastructure/adapters/scenario_catalog.py
@@ -46,8 +46,3 @@ class ScenarioCatalog:
             scenarios.append(scenario)
 
         return scenarios
-
-    def print_scenarios(self):
-        for path in self.root_dir.iterdir():
-            prefix, sce_id, name = path.name.upper().split("_", maxsplit=2)
-            print(f"{prefix}-{sce_id} - {name}")
--- src/zmena/infrastructure/entrypoints/list_scenarios.py
+++ src/zmena/infrastructure/entrypoints/list_scenarios.py
@@ -1,4 +1,5 @@
 from zmena.infrastructure import ScenarioCatalog
 
 catalog = ScenarioCatalog()
-catalog.print_scenarios()
+for scenario in catalog.get_all():
+    print(f"SCE-{scenario.sce_id} - {scenario.name.upper()}")
