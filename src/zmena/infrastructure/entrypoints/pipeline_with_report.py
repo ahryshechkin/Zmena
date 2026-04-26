b56cadaecbf444e3c8fe6a0d2b6786b264af3b75
Andrei Hryshechkin <andrei.hryshechkin@gmail.com>
2026-04-26 12:53:21 +0200

    refactor: make pipeline name more descriptive

--- src/zmena/infrastructure/entrypoints/pipeline_with_report.py
+++ src/zmena/infrastructure/entrypoints/pipeline_with_report.py
@@ -1,11 +1,11 @@
-from zmena.application import Pipeline, ScenarioCatalog
+from zmena.application import AnalysisPipeline, ScenarioCatalog
 from zmena.infrastructure.representation.report import Report
 
 sce_ids = ["313"]
 catalog = ScenarioCatalog()
 for scenario in catalog.get_many(sce_ids):
-    pipeline = Pipeline(scenario.before, scenario.after)
-    result = pipeline.run()
+    analysis_pipeline = AnalysisPipeline(scenario.before, scenario.after)
+    result = analysis_pipeline.run()
 
     report = Report(scenario)
     report.show_scenario()
