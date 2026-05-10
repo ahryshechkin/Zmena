0d8548ee1638245efb3355a85106c80d36598e7a
Andrei Hryshechkin <andrei.hryshechkin@gmail.com>
2026-05-10 20:10:33 +0200

    refactor: set up presets inside steps

--- src/zmena/application/steps/hypothesis_proposer.py
+++ src/zmena/application/steps/hypothesis_proposer.py
@@ -1,13 +1,16 @@
+from zmena.domain.presets.rules import RulePreset
+
+
 class HypothesisProposer:
-    def __init__(self, rules):
-        self.rules = rules
+    def __init__(self):
+        self.preset = RulePreset()
 
     def __repr__(self):
         return "HypothesisProposer(preset=rules)"
 
     def propose(self, bundle):
         hypotheses = []
-        for rule in self.rules:
+        for rule in self.preset.default():
             hypotheses.extend(rule.generate(bundle))
 
         return hypotheses
