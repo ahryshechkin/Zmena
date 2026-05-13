87098d13a8ca239e7783594cae4545eb48a39771
Andrei Hryshechkin <andrei.hryshechkin@gmail.com>
2026-05-13 22:30:09 +0200

    feat: extract Component from Decision

--- src/zmena/domain/model/decision.py
+++ src/zmena/domain/model/decision.py
@@ -1,21 +1,11 @@
-from zmena.domain.model.refinement import Refinement
-
-
 class Decision:
-    def __init__(self, component, preset):
-        self.component = component
-        self.preset = preset
-        self.refined_links = []
+    def __init__(self, refined_links):
+        self.refined_links = refined_links
 
     def __repr__(self):
         return f"Decision(chosen={len(self.chosen())})"
 
     def candidates(self):
-        if not self.refined_links:
-            initial_links = self.component.assess(self.preset.local())
-            refinement = Refinement(initial_links)
-            self.refined_links = refinement.reassess(self.preset.overall())
-
         return self.refined_links
 
     def chosen(self):
