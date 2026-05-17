f6ccf3c853a04feb19be4bdab564d6a92cbebbfe
Andrei Hryshechkin <andrei.hryshechkin@gmail.com>
2026-05-17 21:27:51 +0200

    feat: create lens label

--- src/zmena/domain/lenses/column_swap.py
+++ src/zmena/domain/lenses/column_swap.py
@@ -1,10 +1,10 @@
 from zmena.domain.lenses.lens import Lens
-from zmena.domain.types.heuristic_label import HeuristicLabel
+from zmena.domain.types.lens_label import LensLabel
 
 
 class ColumnSwapLens(Lens):
     def __init__(self):
-        super().__init__(HeuristicLabel.COLUMN_SWAP)
+        super().__init__(LensLabel.COLUMN_SWAP)
 
     def evaluate(self, link, context):
         pass
--- src/zmena/domain/types/heuristic_label.py
+++ src/zmena/domain/types/heuristic_label.py
@@ -3,7 +3,6 @@ from enum import StrEnum
 
 class HeuristicLabel(StrEnum):
     BLOCK_MISMATCH = "block mismatch"
-    COLUMN_SWAP = "column swap"
     NAME_SIMILARITY = "name similarity"
     POSITION_SIMILARITY = "position similarity"
     SIGNATURE_SIMILARITY = "signature similarity"
