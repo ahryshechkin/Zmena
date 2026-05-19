95e67ce42454ac6bbc0419edf4a4eadcb5583841
Andrei Hryshechkin <andrei.hryshechkin@gmail.com>
2026-05-19 23:53:02 +0200

    feat: implement column swap lens logic

--- src/zmena/domain/lenses/column_swap.py
+++ src/zmena/domain/lenses/column_swap.py
@@ -7,6 +7,13 @@ class ColumnSwapLens(Lens):
         super().__init__(LensKind.COLUMN_SWAP)
 
     def evaluate(self, link, context):
-        if not link and not context:
-            pass
+        left, right = link.fragments()
+        if left.same_name_but_different_block_as(right):
+            for first in context.original_links():
+                f_left, f_right = first.fragments()
+                if left.same_name_as(f_left) and not right.same_name_as(f_right):
+                    for second in context.original_links():
+                        s_left, s_right = second.fragments()
+                        if f_left.same_name_as(s_right) and f_right.same_name_as(s_left):
+                            return []
         return []
