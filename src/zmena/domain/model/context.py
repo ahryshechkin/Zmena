32ede5636506d4d62d528dfe08ecf3d4b8b435ee
Andrei Hryshechkin <andrei.hryshechkin@gmail.com>
2026-05-19 22:35:28 +0200

    feat: add __repr__ method to Context

--- src/zmena/domain/model/context.py
+++ src/zmena/domain/model/context.py
@@ -5,6 +5,9 @@ class Context:
     def __init__(self, links):
         self.links = links
 
+    def __repr__(self):
+        return f"Context(links={len(self.links)})"
+
     def original_links(self):
         return self.links
 
