186fbdd61ebf543690dd1bab3e1e4cb97481176d
Andrei Hryshechkin <andrei.hryshechkin@gmail.com>
2026-05-22 22:28:32 +0200

    feat: handle report case when no evidence exists

--- src/zmena/domain/lenses/column_swap.py
+++ src/zmena/domain/lenses/column_swap.py
@@ -9,12 +9,15 @@ class ColumnSwapLens(Lens):
 
     def evaluate(self, link, context):
         left, right = link.fragments()
-        if left.same_name_but_different_block_as(right):
-            for first in context.original_links():
-                f_left, f_right = first.fragments()
-                if left.same_name_as(f_left) and not right.same_name_as(f_right):
-                    for second in context.original_links():
-                        s_left, s_right = second.fragments()
-                        if f_left.same_name_as(s_right) and f_right.same_name_as(s_left):
-                            return [Evidence("", 1.0, 0.9, self.kind)]
+        if not left.same_name_but_different_block_as(right):
+            return []
+
+        for first in context.original_links():
+            f_left, f_right = first.fragments()
+            if left.same_name_as(f_left) and not right.same_name_as(f_right):
+                for second in context.original_links():
+                    s_left, s_right = second.fragments()
+                    if f_left.same_name_as(s_right) and f_right.same_name_as(s_left):
+                        return [Evidence("", 1.0, 0.9, self.kind)]
+
         return []
