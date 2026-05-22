cb8d25d821ffae97d516cfbed9ed257539c53550
Andrei Hryshechkin <andrei.hryshechkin@gmail.com>
2026-05-22 23:23:42 +0200

    feat: inroduce fragment projection

---
+++ src/zmena/infrastructure/representation/projections/fragment.py
@@ -0,0 +1,11 @@
+from zmena.domain.types.tag import Tag
+
+
+class FragmentProjection:
+    def __init__(self, fragment):
+        self.fragment = fragment
+
+    def caption(self):
+        if self.fragment.tag == Tag.STUB:
+            return "Stub"
+        return f"{self.fragment.name} ({self.fragment.side}:{self.fragment.position})"
