ed08a73730564971cb00eb3e2fe754df8afe83d6
Andrei Hryshechkin <andrei.hryshechkin@gmail.com>
2026-07-04 16:11:11 +0200

    test: tune unit tests

--- src/zmena/domain/model/fragments/fragment.py
+++ src/zmena/domain/model/fragments/fragment.py
@@ -8,7 +8,7 @@ class Fragment:
         self.block = block
         self.position = position
         self.side = side
-        self.name = name.lower()
+        self.name = name
         self.data_type = data_type
         self.constraint = constraint
 
--- tests/unit/test_sql_column_profile.py
+++ tests/unit/test_sql_column_profile.py
@@ -7,19 +7,19 @@ class TestSQLColumnProfile(unittest.TestCase):
     def test_all_columns_in_order(self):
         sql_column_profile = SQLColumnProfile("""
         CREATE TABLE t (
-            col_01 int not null,
-            col_02 varchar(50) not null,
-            col_03 varchar(200),
+            col_01 INT NOT NULL,
+            col_02 VARCHAR(50) NOT NULL,
+            col_03 VARCHAR(200),
         );
         """)
 
         self.assertEqual(
-            sql_column_profile.snapshot(),
             [
-                "col_01 int not null",
-                "col_02 varchar(50) not null",
-                "col_03 varchar(200)",
+                "COL_01 INT NOT NULL",
+                "COL_02 VARCHAR(50) NOT NULL",
+                "COL_03 VARCHAR(200)",
             ],
+            sql_column_profile.snapshot(),
         )
 
     def test_empty_table(self):
@@ -28,41 +28,41 @@ class TestSQLColumnProfile(unittest.TestCase):
 
     def test_one_line_definition(self):
         sql_column_profile = SQLColumnProfile("""
-        CREATE TABLE t (col_01 int not null, col_02 varchar(50) not null, col_03 varchar(200));
+        CREATE TABLE t (col_01 INT NOT NULL, col_02 VARCHAR(50) NOT NULL, col_03 VARCHAR(200));
         """)
 
         self.assertEqual(
-            sql_column_profile.snapshot(),
             [
-                "col_01 int not null",
-                "col_02 varchar(50) not null",
-                "col_03 varchar(200)",
+                "COL_01 INT NOT NULL",
+                "COL_02 VARCHAR(50) NOT NULL",
+                "COL_03 VARCHAR(200)",
             ],
+            sql_column_profile.snapshot(),
         )
 
     def test_rough_unaligned_definition(self):
         sql_column_profile = SQLColumnProfile("""
         CREATE TABLE t (
-            col_01 int        not null    ,
-                             col_02 varchar             ( 50) not     null,
-        col_03 varchar(200)
+            col_01 INT        NOT NULL    ,
+                             col_02 VARCHAR             ( 50) not     null,
+        col_03 VARCHAR(200)
         );
         """)
 
         self.assertEqual(
-            sql_column_profile.snapshot(),
             [
-                "col_01 int not null",
-                "col_02 varchar(50) not null",
-                "col_03 varchar(200)",
+                "COL_01 INT NOT NULL",
+                "COL_02 VARCHAR(50) NOT NULL",
+                "COL_03 VARCHAR(200)",
             ],
+            sql_column_profile.snapshot(),
         )
 
     def test_single_column(self):
         sql_column_profile = SQLColumnProfile("""
         CREATE TABLE t (
-            col_01 int not null
+            col_01 INT NOT NULL
         );
         """)
 
-        self.assertEqual(sql_column_profile.snapshot(), ["col_01 int not null"])
+        self.assertEqual(["COL_01 INT NOT NULL"], sql_column_profile.snapshot())
