0a333dae9116a85f9790d92681902f9ab012938a
Andrei Hryshechkin <andrei.hryshechkin@gmail.com>
2026-07-04 15:30:24 +0200

    feat: update package initializer

--- src/zmena/application/__init__.py
+++ src/zmena/application/__init__.py
@@ -1,7 +1,9 @@
 from zmena.application.analysis_pipeline import AnalysisPipeline
 from zmena.application.scenario import Scenario
+from zmena.application.sql_intake_pipeline import SQLIntakePipeline
 
 __all__ = [
     "AnalysisPipeline",
+    "SQLIntakePipeline",
     "Scenario",
 ]
