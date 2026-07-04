94edccd43149b3702ebac5056f64758a37f22da3
Andrei Hryshechkin <andrei.hryshechkin@gmail.com>
2026-07-04 02:15:22 +0200

    feat: add new entrypoints

---
+++ src/zmena/infrastructure/entrypoints/run_sql_intake.py
@@ -0,0 +1,16 @@
+from zmena.application import AnalysisPipeline
+from zmena.infrastructure.adapters.scenario_catalog import ScenarioCatalog
+from zmena.infrastructure.representation.analysis_report import AnalysisReport
+
+sce_ids = ["017"]
+catalog = ScenarioCatalog()
+for scenario in catalog.get_many(sce_ids):
+    analysis_pipeline = AnalysisPipeline(scenario.before, scenario.after)
+    result = analysis_pipeline.run()
+
+    report = AnalysisReport(scenario, result)
+    report.show_scenario()
+    report.show_fragments()
+    report.show_hypotheses()
+    report.show_components()
+    report.show_decisions()
