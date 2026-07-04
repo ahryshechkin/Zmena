89bb9df9d211b7a1582b7a10f53eb2124a9ba868
Andrei Hryshechkin <andrei.hryshechkin@gmail.com>
2026-07-04 16:11:11 +0200

    test: tune unit tests

--- src/zmena/domain/model/column_spec.py
+++ src/zmena/domain/model/column_spec.py
@@ -16,7 +16,7 @@ class ColumnSpec:
         return "ColumnSpec"
 
     def name(self):
-        return self.match.group("name")
+        return self.match.group("name").lower()
 
     def data_type(self):
         return self.match.group("data_type")
