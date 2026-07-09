342837603a35106e9a57892d41ef300e8bf1ffb3
Andrei Hryshechkin <andrei.hryshechkin@gmail.com>
2026-07-09 22:27:36 +0200

    refactor: rename SQLColumnProfile class to SQLTableProfile one

--- src/zmena/domain/sql_intake/sql_table_profile.py
+++ src/zmena/domain/sql_intake/sql_table_profile.py
@@ -1,10 +1,16 @@
 import sqlglot
 
 
-class SQLColumnProfile:
+class SQLTableProfile:
     def __init__(self, ddl):
         self.ddl = ddl
 
+    def __repr__(self):
+        return "SQLTableProfile"
+
     def snapshot(self):
         ast = sqlglot.parse_one(self.ddl)
+
+        for col in ast.find_all(sqlglot.exp.ColumnDef):
+            print(col.sql())
         return [col.sql().upper() for col in ast.find_all(sqlglot.exp.ColumnDef)]
