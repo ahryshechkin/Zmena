e3b5cc202370f952ea64684f44a4231e4b632203
Andrei Hryshechkin <andrei.hryshechkin@gmail.com>
2026-07-09 22:33:04 +0200

    test: swap expected and actual values in component tests

--- tests/component/test_sql_intake_scenarios.py
+++ tests/component/test_sql_intake_scenarios.py
@@ -30,52 +30,52 @@ class TestSQLIntakeScenarios(unittest.TestCase):
         scenario = self.catalog.get("701")
         actual = self.collect_winners(scenario)
 
-        self.assertCountEqual(actual, scenario.expected)
+        self.assertCountEqual(scenario.expected, actual)
 
     def test_sce_702_add_column_neat_before_chaotic_after(self):
         scenario = self.catalog.get("702")
         actual = self.collect_winners(scenario)
 
-        self.assertCountEqual(actual, scenario.expected)
+        self.assertCountEqual(scenario.expected, actual)
 
     def test_sce_703_add_column_chaotic_before_neat_after(self):
         scenario = self.catalog.get("703")
         actual = self.collect_winners(scenario)
 
-        self.assertCountEqual(actual, scenario.expected)
+        self.assertCountEqual(scenario.expected, actual)
 
     def test_sce_704_add_column_single_line_table(self):
         scenario = self.catalog.get("704")
         actual = self.collect_winners(scenario)
 
-        self.assertCountEqual(actual, scenario.expected)
+        self.assertCountEqual(scenario.expected, actual)
 
     def test_sce_705_add_column_neat_before_uppercase_after(self):
         scenario = self.catalog.get("705")
         actual = self.collect_winners(scenario)
 
-        self.assertCountEqual(actual, scenario.expected)
+        self.assertCountEqual(scenario.expected, actual)
 
     def test_sce_706_add_column_neat_before_lowercase_after(self):
         scenario = self.catalog.get("706")
         actual = self.collect_winners(scenario)
 
-        self.assertCountEqual(actual, scenario.expected)
+        self.assertCountEqual(scenario.expected, actual)
 
     def test_sce_707_add_column_neat_before_mixed_after(self):
         scenario = self.catalog.get("707")
         actual = self.collect_winners(scenario)
 
-        self.assertCountEqual(actual, scenario.expected)
+        self.assertCountEqual(scenario.expected, actual)
 
     def test_sce_708_add_column_blank_lines(self):
         scenario = self.catalog.get("708")
         actual = self.collect_winners(scenario)
 
-        self.assertCountEqual(actual, scenario.expected)
+        self.assertCountEqual(scenario.expected, actual)
 
     def test_sce_709_rename_column_single_column_table(self):
         scenario = self.catalog.get("709")
         actual = self.collect_winners(scenario)
 
-        self.assertCountEqual(actual, scenario.expected)
+        self.assertCountEqual(scenario.expected, actual)
