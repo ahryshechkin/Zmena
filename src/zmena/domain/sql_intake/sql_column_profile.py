7c80cc49406ae8ce8f7edfae3fab9fd438356ef0
Andrei Hryshechkin <andrei.hryshechkin@gmail.com>
2026-07-10 23:14:24 +0200

    feat: add __repr__ method

--- src/zmena/domain/sql_intake/sql_column_profile.py
+++ src/zmena/domain/sql_intake/sql_column_profile.py
@@ -1,6 +1,9 @@
 class SQLColumnProfile:
     def __init__(self, definition):
-        self.definition = definition
+        self.definition = definition.sql()
+
+    def __repr__(self):
+        return f"SQLColumnProfile(definition={self.definition})"
 
     def formatted_view(self):
-        return self.definition
+        return self.definition.upper()
