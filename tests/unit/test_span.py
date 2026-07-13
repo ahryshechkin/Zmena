1e9d133ccd714ee5a22cf3ee655f9d3a0e8c08a1
Andrei Hryshechkin <andrei.hryshechkin@gmail.com>
2026-07-13 22:36:10 +0200

    test: update Span unit tests

--- tests/unit/test_span.py
+++ tests/unit/test_span.py
@@ -6,16 +6,16 @@ from zmena.domain.semantic_engine.core.span import Span
 class TestSpan(unittest.TestCase):
     def setUp(self):
         self.before = [
-            "col_01 int not null",
-            "col_02 varchar(50) not null",
-            "col_03 varchar(200)",
-            "col_04 varchar(50) not null",
-            "col_05 varchar(50)",
-            "col_06 int",
-            "col_07 varchar(1) not null",
-            "col_08 date not null",
-            "col_09 datetime2 not null",
-            "col_10 datetime2 not null",
+            "col_01 INT NOT NULL",
+            "col_02 VARCHAR(50) NOT NULL",
+            "col_03 VARCHAR(200)",
+            "col_04 VARCHAR(50) NOT NULL",
+            "col_05 VARCHAR(50)",
+            "col_06 INT",
+            "col_07 VARCHAR(1) NOT NULL",
+            "col_08 DATE NOT NULL",
+            "col_09 DATETIME2 NOT NULL",
+            "col_10 DATETIME2 NOT NULL",
         ]
 
         self.span = Span(self.before, 2, 5)
