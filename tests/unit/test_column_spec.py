3ede83a4bd4f7c1665016b50669c71b9b0b701bd
Andrei Hryshechkin <andrei.hryshechkin@gmail.com>
2026-07-14 22:17:39 +0200

    test: implement ColumnSpec unit tests

--- tests/unit/test_column_spec.py
+++ tests/unit/test_column_spec.py
@@ -1,15 +1,35 @@
 import unittest
 
+from zmena.domain.semantic_engine.core.column_spec import ColumnSpec
+
 
 class TestColumnSpec(unittest.TestCase):
-    def setUp(self):
-        pass
+    def test_constraint_absence(self):
+        column_spec = ColumnSpec("col_03 VARCHAR(200)")
+        self.assertIsNone(column_spec.constraint())
 
-    def test_name(self):
-        pass
+    def test_constraint_not_null(self):
+        column_spec = ColumnSpec("col_01 INT NOT NULL")
+        self.assertEqual("NOT NULL", column_spec.constraint())
+
+    def test_constraint_null(self):
+        column_spec = ColumnSpec("col_01 INT NULL")
+        self.assertEqual("NULL", column_spec.constraint())
 
     def test_data_type(self):
-        pass
+        column_spec = ColumnSpec("col_01 INT NOT NULL")
+        self.assertEqual("INT", column_spec.data_type())
 
-    def test_constraint(self):
-        pass
+    def test_extra_whitespaces(self):
+        column_spec = ColumnSpec("col_01    INT    NOT    NULL")
+        self.assertEqual("col_01", column_spec.name())
+        self.assertEqual("INT", column_spec.data_type())
+        self.assertEqual("NOT NULL", column_spec.constraint())
+
+    def test_name(self):
+        column_spec = ColumnSpec("col_01 INT NOT NULL")
+        self.assertEqual("col_01", column_spec.name())
+
+    def test_repr(self):
+        column_spec = ColumnSpec("col_01 INT NOT NULL")
+        self.assertEqual("ColumnSpec", repr(column_spec))
