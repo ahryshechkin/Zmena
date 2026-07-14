98cfb9ea5815bd1d4986156ffd9705fc952b73f9
Andrei Hryshechkin <andrei.hryshechkin@gmail.com>
2026-07-14 21:55:00 +0200

    test: introduce initial version of ColumnSpec

---
+++ tests/unit/test_column_spec.py
@@ -0,0 +1,15 @@
+import unittest
+
+
+class TestColumnSpec(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_name(self):
+        pass
+
+    def test_data_type(self):
+        pass
+
+    def test_constraint(self):
+        pass
