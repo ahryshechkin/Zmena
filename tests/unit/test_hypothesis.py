2829d67edf3efb9d7ef34da216e08a4ff27322de
Andrei Hryshechkin <andrei.hryshechkin@gmail.com>
2026-07-16 23:28:48 +0200

    test: add initial unit tests for Hypothesis

---
+++ tests/unit/test_hypothesis.py
@@ -0,0 +1,34 @@
+import unittest
+
+# from zmena.domain.semantic_engine.core.hypothesis import Hypothesis
+# from zmena.domain.semantic_engine.fragments.left import LeftFragment
+# from zmena.domain.semantic_engine.fragments.right import RightFragment
+
+
+class TestHypothesis(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_has_block_mismatch(self):
+        pass
+
+    def test_has_same_name(self):
+        pass
+
+    def test_has_same_position(self):
+        pass
+
+    def test_has_same_signature(self):
+        pass
+
+    def test_key(self):
+        pass
+
+    def test_neighbor(self):
+        pass
+
+    def test_repr(self):
+        pass
+
+    def test_str(self):
+        pass
