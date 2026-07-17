daf6aa5a525192d865b5e9d0f21a8b889a5afe0d
Andrei Hryshechkin <andrei.hryshechkin@gmail.com>
2026-07-17 21:44:52 +0200

    test: prepare required test objects

--- src/zmena/infrastructure/entrypoints/run_semantic_engine.py
+++ src/zmena/infrastructure/entrypoints/run_semantic_engine.py
@@ -13,7 +13,7 @@ for scenario in catalog.get_many(sce_ids):
 
     report = AnalysisReport(scenario, result)
     report.show_scenario()
-    report.show_fragments()
-    report.show_hypotheses()
-    report.show_components()
-    report.show_decisions()
+    # report.show_fragments()
+    # report.show_hypotheses()
+    # report.show_components()
+    # report.show_decisions()
--- tests/unit/test_hypothesis.py
+++ tests/unit/test_hypothesis.py
@@ -1,13 +1,34 @@
 import unittest
 
-# from zmena.domain.semantic_engine.core.hypothesis import Hypothesis
-# from zmena.domain.semantic_engine.fragments.left import LeftFragment
-# from zmena.domain.semantic_engine.fragments.right import RightFragment
+from zmena.domain.semantic_engine.core.hypothesis import Hypothesis
+from zmena.domain.semantic_engine.fragments.left import LeftFragment
+from zmena.domain.semantic_engine.fragments.right import RightFragment
+from zmena.domain.semantic_engine.types.rule_kind import RuleKind
+from zmena.domain.semantic_engine.types.side import Side
+from zmena.domain.semantic_engine.types.tag import Tag
 
 
 class TestHypothesis(unittest.TestCase):
     def setUp(self):
-        pass
+        left = object.__new__(LeftFragment)
+        left.tag = Tag.REPLACE
+        left.block = "03050306"
+        left.position = 4
+        left.side = Side.LEFT
+        left.name = "col_04"
+        left.data_type = "VARCHAR(50)"
+        left.constraint = "NOT NULL"
+
+        right = object.__new__(RightFragment)
+        right.tag = Tag.REPLACE
+        right.block = "03050306"
+        right.position = 4
+        right.side = Side.RIGHT
+        right.name = "col_04"
+        right.data_type = "DATE"
+        right.constraint = None
+
+        self.hypothesis = Hypothesis(RuleKind.POSITION, left, right)
 
     def test_has_block_mismatch(self):
         pass
