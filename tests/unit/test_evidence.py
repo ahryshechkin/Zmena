a9a3d235bc44a0d56ad2f3dbd56301b192e176a2
Andrei Hryshechkin <andrei.hryshechkin@gmail.com>
2026-07-18 21:41:12 +0200

    test: add initial unit tests for Evidence

---
+++ tests/unit/test_evidence.py
@@ -0,0 +1,18 @@
+import unittest
+
+
+class TestEvidence(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_score(self):
+        pass
+
+    def test_description(self):
+        pass
+
+    def test_polarity(self):
+        pass
+
+    def test_repr(self):
+        pass
