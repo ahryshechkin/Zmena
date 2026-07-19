69039c3ff220b3eaba6abff0012ad8a0793de1a7
Andrei Hryshechkin <andrei.hryshechkin@gmail.com>
2026-07-20 00:14:26 +0200

    test: add initial unit tests for Link

---
+++ tests/unit/test_link.py
@@ -0,0 +1,24 @@
+import unittest
+
+
+class TestLink(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_add_evidence(self):
+        pass
+
+    def test_fragments(self):
+        pass
+
+    def test_lt(self):
+        pass
+
+    def test_repr(self):
+        pass
+
+    def test_score(self):
+        pass
+
+    def test_str(self):
+        pass
