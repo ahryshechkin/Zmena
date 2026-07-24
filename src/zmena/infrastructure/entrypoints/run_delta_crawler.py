90b64c29a9ad1126e9884fcc6c1daaa9fc1de5c5
Andrei Hryshechkin <andrei.hryshechkin@gmail.com>
2026-07-25 00:15:01 +0200

    feat: implement FSCommand methods

--- src/zmena/infrastructure/entrypoints/run_delta_crawler.py
+++ src/zmena/infrastructure/entrypoints/run_delta_crawler.py
@@ -1,3 +1,7 @@
 from zmena.infrastructure.adapters.commit_catalog import CommitCatalog
+from zmena.infrastructure.adapters.fs_command import FSCommand
 
 catalog = CommitCatalog()
+fs_command = FSCommand()
+fs_command.mkdir("repo")
+fs_command.copy("./../../../../catalog/cmt/cmt_001_create_tab01", "repo")
