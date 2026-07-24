adbefbde9f017f06b1a74a892fca73b5d72f57ac
Andrei Hryshechkin <andrei.hryshechkin@gmail.com>
2026-07-24 23:18:41 +0200

    feat: define CommitCatalog interface

--- src/zmena/infrastructure/adapters/commit_catalog.py
+++ src/zmena/infrastructure/adapters/commit_catalog.py
@@ -4,3 +4,9 @@ from pathlib import Path
 class CommitCatalog:
     def __init__(self):
         self.root_dir = Path(__file__).resolve().parents[4] / "catalog/cmt"
+
+    def build_repo(self):
+        pass
+
+    def cleanup(self):
+        pass
