bcecb5bbf154d31115ea7b16ff5b9aafc412d4a6
Andrei Hryshechkin <andrei.hryshechkin@gmail.com>
2026-07-26 23:23:53 +0200

    feat: define GitCommand interface

--- src/zmena/infrastructure/adapters/git_command.py
+++ src/zmena/infrastructure/adapters/git_command.py
@@ -1,2 +1,12 @@
+import subprocess
+
+
 class GitCommand:
-    pass
+    def __init__(self, path):
+        self.path = path
+
+    def init_repo(self):
+        subprocess.run([r"C:\Program Files\Git\bin\git.exe", "init"], check=True)
+
+    def add_files(self):
+        pass
