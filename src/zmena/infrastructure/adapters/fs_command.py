123ae7956f1eeacff612d144b507bdab021ad993
Andrei Hryshechkin <andrei.hryshechkin@gmail.com>
2026-07-27 22:22:46 +0200

    feat: add path attribute to FSCommand class

--- src/zmena/infrastructure/adapters/fs_command.py
+++ src/zmena/infrastructure/adapters/fs_command.py
@@ -4,15 +4,18 @@ from pathlib import Path
 
 
 class FSCommand:
-    def mkdir(self, path):
-        Path(path).mkdir(parents=True, exist_ok=True)
+    def __init__(self, path):
+        self.path = path
 
-    def rmdir(self, path):
+    def mkdir(self):
+        Path(self.path).mkdir(parents=True, exist_ok=True)
+
+    def rmdir(self):
         def force_remove(func, p, _):
             Path.chmod(p, stat.S_IWRITE)
             func(p)
 
-        shutil.rmtree(Path(path), onerror=force_remove)
+        shutil.rmtree(Path(self.path), onerror=force_remove)
 
-    def copy(self, src, dst):
-        shutil.copytree(src, dst, dirs_exist_ok=True)
+    def copy(self, src):
+        shutil.copytree(src, self.path, dirs_exist_ok=True)
