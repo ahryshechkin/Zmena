f23f34ad3b2b3509fbec2061f5c327f72a2889a6
Andrei Hryshechkin <andrei.hryshechkin@gmail.com>
2026-07-27 21:26:10 +0200

    feat: add additional flag to run command

--- src/zmena/infrastructure/adapters/git_command.py
+++ src/zmena/infrastructure/adapters/git_command.py
@@ -7,15 +7,15 @@ class GitCommand:
 
     def init(self):
         cmds = ["git", "init"]
-        subprocess.run(cmds, check=True)  # noqa: S603
+        subprocess.run(cmds, cwd=self.path, check=True, text=True)  # noqa: S603
 
     def add(self):
         cmds = ["git", "add", "."]
-        subprocess.run(cmds, check=True)  # noqa: S603
+        subprocess.run(cmds, cwd=self.path, check=True, text=True)  # noqa: S603
 
     def diff(self):
         pass
 
     def commit(self):
         cmds = ["git", "commit", "-m", "Add files"]
-        subprocess.run(cmds, check=True)  # noqa: S603
+        subprocess.run(cmds, cwd=self.path, check=True, text=True)  # noqa: S603
