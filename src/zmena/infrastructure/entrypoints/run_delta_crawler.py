1508685e317a05060c0e57f1e4f46e4a3c753dab
Andrei Hryshechkin <andrei.hryshechkin@gmail.com>
2026-07-27 19:58:31 +0200

    feat: implement GitCommand methods

--- src/zmena/infrastructure/entrypoints/run_delta_crawler.py
+++ src/zmena/infrastructure/entrypoints/run_delta_crawler.py
@@ -1,7 +1,15 @@
+import os
+
 from zmena.infrastructure.adapters.commit_catalog import CommitCatalog
 from zmena.infrastructure.adapters.fs_command import FSCommand
+from zmena.infrastructure.adapters.git_command import GitCommand
 
 catalog = CommitCatalog()
 fs_command = FSCommand()
 fs_command.mkdir("repo")
 fs_command.copy("./../../../../catalog/cmt/cmt_001_create_tab01", "repo")
+os.chdir("repo")
+git_command = GitCommand(".")
+git_command.init_repo()
+# os.chdir("..")
+# fs_command.rmdir("repo")
