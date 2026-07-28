bf060636084c444d1cc2957c57578eb1d853cf0b
Andrei Hryshechkin <andrei.hryshechkin@gmail.com>
2026-07-29 00:23:35 +0200

    feat: add script to create temporary repository

--- src/zmena/infrastructure/entrypoints/run_delta_crawler.py
+++ src/zmena/infrastructure/entrypoints/run_delta_crawler.py
@@ -1,27 +1,4 @@
 from zmena.infrastructure.adapters.commit_catalog import CommitCatalog
-from zmena.infrastructure.adapters.fs_command import FSCommand
-from zmena.infrastructure.adapters.git_command import GitCommand
-from zmena.infrastructure.project_directory import ProjectDirectory
 
 catalog = CommitCatalog()
-directory = ProjectDirectory()
-fs_command = FSCommand(directory.test_repo())
-git_command = GitCommand(directory.test_repo())
-fs_command.mkdir()
-git_command.init()
-fs_command.copy(f"{directory.cmt()}/cmt_001_create_tab01")
-git_command.add()
-git_command.commit()
-fs_command.copy(f"{directory.cmt()}/cmt_002_add_column_tab01")
-git_command.add()
-git_command.commit()
-fs_command.copy(f"{directory.cmt()}/cmt_003_create_tab02")
-git_command.add()
-git_command.commit()
-fs_command.copy(f"{directory.cmt()}/cmt_004_rename_column_tab02")
-git_command.add()
-git_command.commit()
-fs_command.copy(f"{directory.cmt()}/cmt_005_update_tab01_and_tab02")
-git_command.add()
-git_command.commit()
-fs_command.rmdir()
+catalog.build_repo()
