91e9263265d31e808c04737effdb4134d228b81e
Andrei Hryshechkin <andrei.hryshechkin@gmail.com>
2026-08-03 23:36:23 +0200

    refactor: set up DeltaCrawler entrypoint

--- src/zmena/infrastructure/entrypoints/run_delta_crawler.py
+++ src/zmena/infrastructure/entrypoints/run_delta_crawler.py
@@ -1,8 +1,12 @@
 from zmena.application.delta_crawler_pipeline import DeltaCrawlerPipeline
 from zmena.infrastructure.adapters.commit_catalog import CommitCatalog
+from zmena.infrastructure.adapters.git_command import GitCommand
+from zmena.infrastructure.project_directory import ProjectDirectory
 
 catalog = CommitCatalog()
-# catalog.build_repo()
-# catalog.cleanup()
-pipeline = DeltaCrawlerPipeline()
-pipeline.run()
+# catalog.build_demo_repo()
+directory = ProjectDirectory()
+command = GitCommand(directory.demo_repo())
+pipeline = DeltaCrawlerPipeline("51a59be", "3aadd15")
+pipeline.run(command)
+# catalog.cleanup_demo_repo()
