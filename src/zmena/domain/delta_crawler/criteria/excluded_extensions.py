4a3b81417f070a6db552f9df735c555d022355bf
Andrei Hryshechkin <andrei.hryshechkin@gmail.com>
2026-08-18 19:33:21 +0200

    feat: implement ExcludedExtensionsCriterion

--- src/zmena/domain/delta_crawler/criteria/excluded_extensions.py
+++ src/zmena/domain/delta_crawler/criteria/excluded_extensions.py
@@ -2,8 +2,12 @@ from zmena.domain.delta_crawler.criteria.criterion import Criterion
 
 
 class ExcludedExtensionsCriterion(Criterion):
-    def __init__(self):
-        pass
+    def __init__(self, excluded_extensions):
+        self.excluded_extensions = excluded_extensions
 
     def apply(self, paths):
-        pass
+        return [
+            path
+            for path in paths
+            if not any(path.endswith(extension) for extension in self.excluded_extensions)
+        ]
