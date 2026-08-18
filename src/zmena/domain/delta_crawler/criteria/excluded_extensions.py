dc75be822d7806bd89400509cf2492a8b569f9cc
Andrei Hryshechkin <andrei.hryshechkin@gmail.com>
2026-08-18 19:17:59 +0200

    feat: add ExcludedExtensionsCriterion class

---
+++ src/zmena/domain/delta_crawler/criteria/excluded_extensions.py
@@ -0,0 +1,9 @@
+from zmena.domain.delta_crawler.criteria.criterion import Criterion
+
+
+class ExcludedExtensionsCriterion(Criterion):
+    def __init__(self):
+        pass
+
+    def apply(self, paths):
+        pass
