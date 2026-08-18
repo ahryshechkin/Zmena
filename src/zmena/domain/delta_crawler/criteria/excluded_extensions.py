f1e34b798f1dc87039a005f05a3d66b9ceed25cd
Andrei Hryshechkin <andrei.hryshechkin@gmail.com>
2026-08-18 21:57:34 +0200

    feat: add IncludedExtensionsCriterion class

--- src/zmena/domain/delta_crawler/criteria/excluded_extensions.py
+++ src/zmena/domain/delta_crawler/criteria/excluded_extensions.py
@@ -1,9 +1,10 @@
 from zmena.domain.delta_crawler.criteria.criterion import Criterion
+from zmena.domain.delta_crawler.kinds.criterion_kind import CriterionKind
 
 
 class ExcludedExtensionsCriterion(Criterion):
     def __init__(self, excluded_extensions):
-        super().__init__("dad")
+        super().__init__(CriterionKind.EXCLUDED_EXTENSIONS)
         self.excluded_extensions = excluded_extensions
 
     def apply(self, paths):
