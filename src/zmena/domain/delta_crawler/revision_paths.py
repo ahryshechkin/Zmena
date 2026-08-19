a0a97adcca49dccabdb63cac6a1e0c5037e5203d
Andrei Hryshechkin <andrei.hryshechkin@gmail.com>
2026-08-19 22:23:49 +0200

    feat: apply default criterion preset

--- src/zmena/domain/delta_crawler/revision_paths.py
+++ src/zmena/domain/delta_crawler/revision_paths.py
@@ -1,10 +1,14 @@
+from zmena.domain.delta_crawler.presets.criteria import CriterionPreset
+
+
 class RevisionPaths:
     def __init__(self, paths):
         self.paths = paths
+        self.preset = CriterionPreset()
 
-    def filter(self, criteria):
+    def filter(self):
         selected_paths = self.paths
-        for criterion in criteria:
+        for criterion in self.preset.default():
             selected_paths = criterion.apply(selected_paths)
 
         return selected_paths
