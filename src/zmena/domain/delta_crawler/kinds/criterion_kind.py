c628c12181c5f15f3fa90e0ade5577380bce44cf
Andrei Hryshechkin <andrei.hryshechkin@gmail.com>
2026-08-20 20:41:31 +0200

    feat: extend CriterionKind

--- src/zmena/domain/delta_crawler/kinds/criterion_kind.py
+++ src/zmena/domain/delta_crawler/kinds/criterion_kind.py
@@ -3,4 +3,6 @@ from enum import StrEnum
 
 class CriterionKind(StrEnum):
     EXCLUDED_EXTENSIONS = "excluded extensions"
+    EXCLUDED_DIRECTORIES = "excluded directories"
     INCLUDED_EXTENSIONS = "included extensions"
+    INCLUDED_DIRECTORIES = "included directories"
