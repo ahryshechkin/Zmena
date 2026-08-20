fe9b754bc59ce8e2e967e7888f2cb0163ecd6251
Andrei Hryshechkin <andrei.hryshechkin@gmail.com>
2026-08-20 22:52:21 +0200

    feat: add ExcludedDirectories criterion

---
+++ src/zmena/domain/delta_crawler/criteria/excluded_directories.py
@@ -0,0 +1,15 @@
+from zmena.domain.delta_crawler.criteria.criterion import Criterion
+from zmena.domain.delta_crawler.kinds.criterion_kind import CriterionKind
+
+
+class ExcludedDirectoriesCriterion(Criterion):
+    def __init__(self, excluded_directories):
+        super().__init__(CriterionKind.EXCLUDED_DIRECTORIES)
+        self.excluded_directories = excluded_directories
+
+    def apply(self, paths):
+        return [
+            path
+            for path in paths
+            if not any(directory in path for directory in self.excluded_directories)
+        ]
--- src/zmena/domain/delta_crawler/presets/criteria.py
+++ src/zmena/domain/delta_crawler/presets/criteria.py
@@ -1,3 +1,4 @@
+from zmena.domain.delta_crawler.criteria.excluded_directories import ExcludedDirectoriesCriterion
 from zmena.domain.delta_crawler.criteria.excluded_extensions import ExcludedExtensionsCriterion
 from zmena.domain.delta_crawler.criteria.included_directories import IncludedDirectoriesCriterion
 from zmena.domain.delta_crawler.criteria.included_extensions import IncludedExtensionsCriterion
@@ -8,9 +9,15 @@ from zmena.domain.delta_crawler.presets.preset import Preset
 class CriterionPreset(Preset):
     def __init__(self):
         super().__init__(PresetKind.CRITERIA)
+        self.excluded_directories = ExcludedDirectoriesCriterion([".sql"])
         self.excluded_extensions = ExcludedExtensionsCriterion([])
         self.included_directories = IncludedDirectoriesCriterion([".sql"])
         self.included_extensions = IncludedExtensionsCriterion([".sql"])
 
     def default(self):
-        return [self.excluded_extensions, self.included_directories, self.included_extensions]
+        return [
+            self.excluded_directories,
+            self.excluded_extensions,
+            self.included_directories,
+            self.included_extensions,
+        ]
