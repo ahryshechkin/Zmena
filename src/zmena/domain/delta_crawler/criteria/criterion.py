d66307c701ab4ff6bcb08286e21f63471f8f7305
Andrei Hryshechkin <andrei.hryshechkin@gmail.com>
2026-08-21 23:18:43 +0200

    test: add ExcludedDirectoriesCriterion tests

--- src/zmena/domain/delta_crawler/criteria/criterion.py
+++ src/zmena/domain/delta_crawler/criteria/criterion.py
@@ -1,13 +1,9 @@
-from abc import ABC, abstractmethod
-
-
-class Criterion(ABC):
+class Criterion:
     def __init__(self, kind):
         self.kind = kind
 
     def __repr__(self):
         return f"Criterion(kind={self.kind})"
 
-    @abstractmethod
     def apply(self, paths):
         pass
--- src/zmena/domain/delta_crawler/presets/criteria.py
+++ src/zmena/domain/delta_crawler/presets/criteria.py
@@ -1,4 +1,7 @@
+from zmena.domain.delta_crawler.criteria.excluded_directories import ExcludedDirectoriesCriterion
 from zmena.domain.delta_crawler.criteria.excluded_extensions import ExcludedExtensionsCriterion
+from zmena.domain.delta_crawler.criteria.included_directories import IncludedDirectoriesCriterion
+from zmena.domain.delta_crawler.criteria.included_extensions import IncludedExtensionsCriterion
 from zmena.domain.delta_crawler.kinds.preset_kind import PresetKind
 from zmena.domain.delta_crawler.presets.preset import Preset
 
@@ -6,15 +9,15 @@ from zmena.domain.delta_crawler.presets.preset import Preset
 class CriterionPreset(Preset):
     def __init__(self):
         super().__init__(PresetKind.CRITERIA)
-        # self.excluded_directories = ExcludedDirectoriesCriterion(["sql"])
-        self.excluded_extensions = ExcludedExtensionsCriterion(["sql"])
-        # self.included_directories = IncludedDirectoriesCriterion([".sql"])
-        # self.included_extensions = IncludedExtensionsCriterion([".sql"])
+        self.excluded_directories = ExcludedDirectoriesCriterion([])
+        self.excluded_extensions = ExcludedExtensionsCriterion([])
+        self.included_directories = IncludedDirectoriesCriterion([])
+        self.included_extensions = IncludedExtensionsCriterion([])
 
     def default(self):
         return [
-            # self.excluded_directories,
+            self.excluded_directories,
             self.excluded_extensions,
-            # self.included_directories,
-            # self.included_extensions,
+            self.included_directories,
+            self.included_extensions,
         ]
