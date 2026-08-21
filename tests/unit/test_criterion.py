953d8c9437755ed4bd98f82671aa89f05751d6fb
Andrei Hryshechkin <andrei.hryshechkin@gmail.com>
2026-08-21 23:18:43 +0200

    test: add ExcludedDirectoriesCriterion tests

--- tests/unit/test_criterion.py
+++ tests/unit/test_criterion.py
@@ -1,10 +1,32 @@
 import unittest
 
+from zmena.domain.delta_crawler.criteria.criterion import Criterion
 from zmena.domain.delta_crawler.criteria.excluded_directories import ExcludedDirectoriesCriterion
 from zmena.domain.delta_crawler.kinds.criterion_kind import CriterionKind
 
 
-class TestExcludedDirectionsCriterion(unittest.TestCase):
+class TestCriterion(unittest.TestCase):
     def test_repr(self):
-        criterion = ExcludedDirectoriesCriterion(CriterionKind.EXCLUDED_DIRECTORIES)
+        criterion = Criterion(CriterionKind.EXCLUDED_DIRECTORIES)
         self.assertEqual("Criterion(kind=excluded directories)", repr(criterion))
+
+
+class TestExcludedDirectoriesCriterion(unittest.TestCase):
+    def test_apply(self):
+        criterion = ExcludedDirectoriesCriterion([""])
+
+        expected = []
+        actual = criterion.apply(
+            [
+                "code/scripts/script01.py",
+                "code/scripts/sql.txt",
+                "code/domain/sql.sql",
+                "infra/catalog/schema/tab01.sql",
+                "infra/catalog/sql/tab02.sql",
+                "infra/catalog/sql/script02.py",
+                "text01.txt",
+                "text02.sql",
+            ]
+        )
+
+        self.assertCountEqual(expected, actual)
