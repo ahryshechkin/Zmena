2000fdb1c3ca9932969966bf5bd3fbf6d1d80160
Andrei Hryshechkin <andrei.hryshechkin@gmail.com>
2026-08-28 23:52:29 +0200

    feat: create Migration class

--- src/zmena/domain/migration_forge/core/migration.py
+++ src/zmena/domain/migration_forge/core/migration.py
@@ -1,3 +1,23 @@
+from zmena.domain.migration_forge.operations.add_column import AddColumn
+from zmena.domain.migration_forge.operations.alter_data_type import AlterDataType
+from zmena.domain.migration_forge.operations.drop_column import DropColumn
+from zmena.domain.migration_forge.operations.rename_column import RenameColumn
+
+
 class Migration:
     def plan(self, change):
-        pass
+        if change.is_add():
+            return [AddColumn(change.after.name, change.after.data_type, change.after.nullable)]
+
+        if change.is_drop():
+            return [DropColumn(change.before.name)]
+
+        operations = []
+        if change.has_name_change():
+            operations.append(RenameColumn(change.before.name, change.after.name))
+        if change.has_data_type_change():
+            operations.append(
+                AlterDataType(change.before.name, change.before.data_type, change.after.data_type)
+            )
+
+        return operations
