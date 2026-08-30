615d73e9b83c461f51524f26f29abe2dc24a5fb8
Andrei Hryshechkin <andrei.hryshechkin@gmail.com>
2026-08-30 22:38:44 +0200

    feat: add __post_init__ method to define statement kind

--- src/zmena/domain/migration_forge/statements/add_column.py
+++ src/zmena/domain/migration_forge/statements/add_column.py
@@ -1,8 +1,13 @@
 from dataclasses import dataclass
 
+from zmena.domain.migration_forge.kinds.statement import StatementKind
+
 
 @dataclass(frozen=True)
 class AddColumnStatement:
     name: str
     data_type: str
     nullable: bool
+
+    def __post_init__(self):
+        super().__init__(StatementKind.ADD_COLUMN)
--- src/zmena/domain/migration_forge/statements/set_not_null.py
+++ src/zmena/domain/migration_forge/statements/set_not_null.py
@@ -1,6 +1,11 @@
 from dataclasses import dataclass
 
+from zmena.domain.migration_forge.kinds.statement import StatementKind
+
 
 @dataclass(frozen=True)
 class SetNotNullStatement:
     name: str
+
+    def __post_init__(self):
+        super().__init__(StatementKind.SET_NOT_NULL)
