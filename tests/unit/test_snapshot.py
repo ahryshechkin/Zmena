c9a9e89c1b29df8d4d2fa9b0d502ecb69ffa591a
Andrei Hryshechkin <andrei.hryshechkin@gmail.com>
2026-09-01 23:26:52 +0200

    test: add unit tests for Snapshot class

---
+++ tests/unit/test_snapshot.py
@@ -0,0 +1,16 @@
+import unittest
+
+from zmena.domain.migration_forge.core.snapshot import Snapshot
+
+
+class TestSnapshot(unittest.TestCase):
+    def setUp(self):
+        self.snapshot = Snapshot(name="col_88", data_type="DATE", nullable=False)
+
+    def test_init(self):
+        self.assertEqual("col_88", self.snapshot.name)
+        self.assertEqual("DATE", self.snapshot.data_type)
+        self.assertFalse(self.snapshot.nullable)
+
+    def test_repr(self):
+        self.assertEqual("Snapshot(name=col_88,data_type=DATE,nullable=False)", repr(self.snapshot))
