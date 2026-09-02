0db87a6c0555f7b4ca084e9ecf8d0ec87dafac4a
Andrei Hryshechkin <andrei.hryshechkin@gmail.com>
2026-09-03 00:33:43 +0200

    test: create SQLColumnProfile unit tests

--- src/zmena/domain/sql_intake/sql_column_profile.py
+++ src/zmena/domain/sql_intake/sql_column_profile.py
@@ -1,10 +1,10 @@
 class SQLColumnProfile:
-    def __init__(self, column_def):
-        self.column_def = column_def.sql()
+    def __init__(self, definition):
+        self.definition = definition.sql()
 
     def __repr__(self):
-        return f"SQLColumnProfile(definition={self.column_def})"
+        return f"SQLColumnProfile(definition={self.definition})"
 
     def formatted_sql(self):
-        name, rest = self.column_def.split(maxsplit=1)
+        name, rest = self.definition.split(maxsplit=1)
         return f"{name.lower()} {rest.upper()}"
---
+++ tests/unit/test_sql_column_profile.py
@@ -0,0 +1,41 @@
+import unittest
+from unittest.mock import Mock
+
+from zmena.domain.sql_intake.sql_column_profile import SQLColumnProfile
+
+
+class TestSQLColumnProfile(unittest.TestCase):
+    def setUp(self):
+        self.column_def = Mock()
+        self.expected = "col_01 INT NOT NULL"
+
+    def test_formatted_sql_lowercase(self):
+        self.column_def.sql.return_value = "col_01 int not null"
+
+        profile = SQLColumnProfile(self.column_def)
+        actual = profile.formatted_sql()
+
+        self.assertEqual(self.expected, actual)
+
+    def test_formatted_sql_mixed(self):
+        self.column_def.sql.return_value = "COL_01 int NoT nuLL"
+
+        profile = SQLColumnProfile(self.column_def)
+        actual = profile.formatted_sql()
+
+        self.assertEqual(self.expected, actual)
+
+    def test_formatted_sql_uppercase(self):
+        self.column_def.sql.return_value = "COL_01 INT NOT NULL"
+
+        profile = SQLColumnProfile(self.column_def)
+        actual = profile.formatted_sql()
+
+        self.assertEqual(self.expected, actual)
+
+    def test_repr(self):
+        self.column_def.sql.return_value = self.expected
+
+        profile = SQLColumnProfile(self.column_def)
+
+        self.assertEqual("SQLColumnProfile(definition=col_01 INT NOT NULL)", repr(profile))
