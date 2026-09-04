42d89ac6e2e6047ba32fd2047a1000f546027462
Andrei Hryshechkin <andrei.hryshechkin@gmail.com>
2026-09-04 22:39:17 +0200

    refactor: clarify ProjectDirectory names

--- src/zmena/infrastructure/project_directory.py
+++ src/zmena/infrastructure/project_directory.py
@@ -23,8 +23,8 @@ class ProjectDirectory:
     def sce(self):
         return self.root() / "catalog/sce"
 
-    def fixtures_se(self):
-        return self.root() / "tests/component/fixtures/se"
+    def se_fixtures(self):
+        return self.root() / "tests/component/fixtures/semantic_engine"
 
     def test_repo(self):
         return self.root() / "tests/integration/test_repo"
