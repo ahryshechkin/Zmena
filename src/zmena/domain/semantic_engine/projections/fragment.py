fc008512538092e398c07de79054b6c83451c8ac
Andrei Hryshechkin <andrei.hryshechkin@gmail.com>
2026-09-06 22:58:17 +0200

    feat: introduce base Snapshot and Projection classes

--- src/zmena/domain/semantic_engine/projections/fragment.py
+++ src/zmena/domain/semantic_engine/projections/fragment.py
@@ -1,7 +1,12 @@
+from zmena.domain.semantic_engine.kinds.projection import ProjectionKind
+from zmena.domain.semantic_engine.projections.projection import Projection
 from zmena.domain.semantic_engine.snapshots.fragment import FragmentSnapshot
 
 
-class FragmentProjection:
+class FragmentProjection(Projection):
+    def __init__(self):
+        super().__init__(ProjectionKind.FRAGMENT)
+
     def from_domain(self, fragment):
         return FragmentSnapshot(
             name=fragment.name,
