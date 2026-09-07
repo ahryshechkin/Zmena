317be831a13eb45fcc8fdcbb850fc533b890c53f
Andrei Hryshechkin <andrei.hryshechkin@gmail.com>
2026-09-07 22:38:43 +0200

    feat: restructure Snapshot as State

---
+++ src/zmena/domain/migration_forge/snapshots/state.py
@@ -0,0 +1,17 @@
+from dataclasses import dataclass
+
+from zmena.domain.migration_forge.kinds.snapshot import SnapshotKind
+from zmena.domain.migration_forge.snapshots.snapshot import Snapshot
+
+
+@dataclass
+class State(Snapshot):
+    name: str
+    data_type: str
+    nullable: bool
+
+    def __repr__(self):
+        return f"State(name={self.name},data_type={self.data_type},nullable={self.nullable})"
+
+    def __post_init__(self):
+        super().__init__(SnapshotKind.STATE)
