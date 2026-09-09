b37eb21812c12e1a0906fc56118e680899d86fdf
Andrei Hryshechkin <andrei.hryshechkin@gmail.com>
2026-09-09 21:19:39 +0200

    fix: update tests according to recent changes

--- tests/unit/test_plan.py
+++ tests/unit/test_plan.py
@@ -3,7 +3,7 @@ from unittest.mock import Mock
 
 from zmena.domain.migration_forge.core.change import Change
 from zmena.domain.migration_forge.core.plan import Plan
-from zmena.domain.migration_forge.snapshots.state import Snapshot
+from zmena.domain.migration_forge.snapshots.state import StateSnapshot
 from zmena.domain.migration_forge.statements.add_column import AddColumnStatement
 from zmena.domain.migration_forge.statements.alter_data_type import AlterDataTypeStatement
 from zmena.domain.migration_forge.statements.drop_column import DropColumnStatement
@@ -18,7 +18,7 @@ class TestPlan(unittest.TestCase):
             AddColumnStatement(name="col_08", data_type="DATE", nullable=False),
         ]
 
-        snapshot = Snapshot(name="col_08", data_type="DATE", nullable=False)
+        snapshot = StateSnapshot(name="col_08", data_type="DATE", nullable=False)
         change = Change(None, snapshot)
         plan = Plan(change)
         actual = plan.derive()
@@ -30,8 +30,8 @@ class TestPlan(unittest.TestCase):
             AlterDataTypeStatement(name="col_08", old_data_type="DATE", new_data_type="TIMESTAMP"),
         ]
 
-        before = Snapshot(name="col_08", data_type="DATE", nullable=False)
-        after = Snapshot(name="col_08", data_type="TIMESTAMP", nullable=False)
+        before = StateSnapshot(name="col_08", data_type="DATE", nullable=False)
+        after = StateSnapshot(name="col_08", data_type="TIMESTAMP", nullable=False)
         change = Change(before, after)
         plan = Plan(change)
         actual = plan.derive()
@@ -43,7 +43,7 @@ class TestPlan(unittest.TestCase):
             DropColumnStatement(name="col_08"),
         ]
 
-        snapshot = Snapshot(name="col_08", data_type="DATE", nullable=False)
+        snapshot = StateSnapshot(name="col_08", data_type="DATE", nullable=False)
         change = Change(snapshot, None)
         plan = Plan(change)
         actual = plan.derive()
@@ -55,8 +55,8 @@ class TestPlan(unittest.TestCase):
             DropNotNullStatement(name="col_08"),
         ]
 
-        before = Snapshot(name="col_08", data_type="DATE", nullable=False)
-        after = Snapshot(name="col_08", data_type="DATE", nullable=True)
+        before = StateSnapshot(name="col_08", data_type="DATE", nullable=False)
+        after = StateSnapshot(name="col_08", data_type="DATE", nullable=True)
         change = Change(before, after)
         plan = Plan(change)
         actual = plan.derive()
@@ -70,8 +70,8 @@ class TestPlan(unittest.TestCase):
             RenameColumnStatement(old_name="col_08", new_name="col_88"),
         ]
 
-        before = Snapshot(name="col_08", data_type="DATE", nullable=False)
-        after = Snapshot(name="col_88", data_type="TIMESTAMP", nullable=True)
+        before = StateSnapshot(name="col_08", data_type="DATE", nullable=False)
+        after = StateSnapshot(name="col_88", data_type="TIMESTAMP", nullable=True)
         change = Change(before, after)
         plan = Plan(change)
         actual = plan.derive()
@@ -83,8 +83,8 @@ class TestPlan(unittest.TestCase):
             RenameColumnStatement(old_name="col_08", new_name="col_88"),
         ]
 
-        before = Snapshot(name="col_08", data_type="DATE", nullable=False)
-        after = Snapshot(name="col_88", data_type="DATE", nullable=False)
+        before = StateSnapshot(name="col_08", data_type="DATE", nullable=False)
+        after = StateSnapshot(name="col_88", data_type="DATE", nullable=False)
         change = Change(before, after)
         plan = Plan(change)
         actual = plan.derive()
@@ -96,8 +96,8 @@ class TestPlan(unittest.TestCase):
             SetNotNullStatement(name="col_08"),
         ]
 
-        before = Snapshot(name="col_08", data_type="DATE", nullable=True)
-        after = Snapshot(name="col_08", data_type="DATE", nullable=False)
+        before = StateSnapshot(name="col_08", data_type="DATE", nullable=True)
+        after = StateSnapshot(name="col_08", data_type="DATE", nullable=False)
         change = Change(before, after)
         plan = Plan(change)
         actual = plan.derive()
