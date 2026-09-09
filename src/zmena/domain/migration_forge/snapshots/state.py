9dc9c6842b4f164383cbc75e7855c55e86a56dcd
Andrei Hryshechkin <andrei.hryshechkin@gmail.com>
2026-09-09 20:59:19 +0200

    feat: reconfigure MigrationForge pieline

--- src/zmena/domain/migration_forge/snapshots/state.py
+++ src/zmena/domain/migration_forge/snapshots/state.py
@@ -11,7 +11,9 @@ class StateSnapshot(Snapshot):
     nullable: bool
 
     def __repr__(self):
-        return f"State(name={self.name},data_type={self.data_type},nullable={self.nullable})"
+        return (
+            f"StateSnapshot(name={self.name},data_type={self.data_type},nullable={self.nullable})"
+        )
 
     def __post_init__(self):
         super().__init__(SnapshotKind.STATE)
