399efe6d1b63f6ccb2da58fddc48b4e28242f4fc
Andrei Hryshechkin <andrei.hryshechkin@gmail.com>
2026-09-14 22:06:55 +0200

    feat: extend Fragment bridge

--- src/zmena/application/bridges/fragment.py
+++ src/zmena/application/bridges/fragment.py
@@ -9,7 +9,10 @@ class FragmentBridge(Bridge):
 
     def translate(self, fragment):
         return FragmentSnapshot(
-            tag=fragment.tag,
+            tag=fragment.tag.value,
+            block=fragment.block,
+            position=fragment.position,
+            side=fragment.side,
             name=fragment.name,
             data_type=fragment.data_type,
             nullable=fragment.constraint != "NOT NULL",
