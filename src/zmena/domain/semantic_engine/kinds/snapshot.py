bf3ed2017e3bcbfd8ed18d3f37697e6f66be5fba
Andrei Hryshechkin <andrei.hryshechkin@gmail.com>
2026-09-15 20:11:04 +0200

    feat: build Hypothesis snapshot

--- src/zmena/domain/semantic_engine/kinds/snapshot.py
+++ src/zmena/domain/semantic_engine/kinds/snapshot.py
@@ -4,5 +4,6 @@ from enum import StrEnum
 class SnapshotKind(StrEnum):
     DECISION = "decision"
     FRAGMENT = "fragment"
+    HYPOTHESIS = "hypothesis"
     LINK = "link"
     UNDEFINED = "undefined"
